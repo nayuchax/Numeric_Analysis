c0a0efd9019bd8bf46b1bd864e8fee93ddd3037c
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-07-05 18:49:31 +0900

    file format

--- src/Piecewise_quadrature.py
+++ src/Piecewise_quadrature.py
@@ -4,7 +4,7 @@ import numpy as np
 # 区分求積法
 def piecewise_quadrature(LIMIT: int, MAX: int, DIVIDE: int) -> float:
     s = 0
-    h = float(1 / DIVIDE)
+    h = float((MAX-LIMIT) / DIVIDE)
 
     divide_ndarr = np.append(np.linspace(LIMIT, MAX, DIVIDE, endpoint=False), MAX)
     divide_list = divide_ndarr.tolist()
